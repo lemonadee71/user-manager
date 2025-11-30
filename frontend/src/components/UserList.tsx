6b3f5f1a3461787e6ce3a223986dc3ab5b66ce92
Shin Andrei Riego <riegoshinandrei@gmail.com>
2025-11-30 19:57:24 +0800

    Add basic text search using match-sorter
    Checks all fields

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import {
   Paper,
@@ -7,10 +8,13 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from '@mui/material';
+import { matchSorter } from 'match-sorter';
 import type { User } from '../types';
 
 const UserList = () => {
+  const [searchText, setSearchText] = useState('');
   const query = useQuery({
     queryKey: ['users'],
     queryFn: async () => {
@@ -24,29 +28,44 @@ const UserList = () => {
 
       return users;
     },
+    placeholderData: [],
   });
+  const allUsers = query.data ?? [];
+  const filteredUsers = searchText
+    ? matchSorter(allUsers, searchText, { keys: ['name', 'username', 'email'] })
+    : allUsers;
 
   return (
-    <TableContainer className="max-w-5xl mx-auto" component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell className="font-bold">Name</TableCell>
-            <TableCell className="font-bold">Username</TableCell>
-            <TableCell className="font-bold">Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {query.data?.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.username}</TableCell>
-              <TableCell>{user.email}</TableCell>
+    <div className="max-w-5xl mx-auto">
+      <TextField
+        id="search"
+        label="Search"
+        variant="outlined"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="mb-4"
+      />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell className="font-bold">Name</TableCell>
+              <TableCell className="font-bold">Username</TableCell>
+              <TableCell className="font-bold">Email</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {filteredUsers.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
